3f8ed5e4c245cb6caa6d82731facb19d9a3fdb87
baltermia <54800864+baltermia@users.noreply.github.com>
2023-10-04 19:45:18 +0200

    Create Authentication Models

---
+++ Core/Data/Models/Authentication/AuthenticatedUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Humteria.Data.Models.Authentication;
+
+public class AuthenticatedUser
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? AccessToken { get; set; }
+}
---
+++ Core/Data/Models/Authentication/UserAuthentication.cs
@@ -0,0 +1,7 @@
+﻿namespace Humteria.Data.Models.Authentication;
+
+public class UserAuthentication
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
